3280ccc62c250857560588bcb3d045eeba740a1b
Crispitipina <68116083+Mama-simba@users.noreply.github.com>
2021-04-16 18:37:54 +0200

    Search Engine added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import Dictionary from "./Dictionary";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hola</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>What word do you want to look up?</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+      <span className="copyright">©️ Coded by Cristina Padilla</span>
+      <a href="https://github.com/Mama-simba/react-weather-app" target="_blank" rel="noreferrer">
+        <i className="fab fa-github-square"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/cristina-padilla-plasencia/"
+        target="_blank" rel="noreferrer"
+      >
+        <i className="fab fa-linkedin"></i>
+      </a>
+    </footer>
+      </div>
     </div>
   );
 }
